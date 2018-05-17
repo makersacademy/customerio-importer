54f0387ae4491eefbda20dc3773b1c0e928e44ed
Sam Morgan <s_morgan@me.com>
2018-05-17 18:36:26 +0100

    maybe strings will work?

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,14 +2,14 @@ require "#{Rails.root}/lib/customerio/contact"
 
 class ContactsController < ApplicationController
   def opt_in
-    contact = Contact.find_by_random_opt_string(params[:opt_in_uuid])
+    contact = Contact.find_by_random_opt_string(params["opt_in_uuid"])
     @success = CustomerIO::Contact.new(contact).opt_in
   rescue
     redirect_to error_url
   end
 
   def opt_out
-    contact = Contact.find_by_random_opt_string(params[:opt_in_uuid])
+    contact = Contact.find_by_random_opt_string(params["opt_in_uuid"])
     @success = CustomerIO::Contact.new(contact).opt_out
   rescue
     redirect_to error_url
