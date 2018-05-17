ef2b21ec1227bbb9965ddfd11e7a7a48564c058f
Sam Morgan <s_morgan@me.com>
2018-05-17 18:56:29 +0100

    Get more vis

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,14 +2,18 @@ require "#{Rails.root}/lib/customerio/contact"
 
 class ContactsController < ApplicationController
   def opt_in
+    p params
     contact = Contact.find_by_random_opt_string(params["opt_in_uuid"])
+    p contact
     @success = CustomerIO::Contact.new(contact).opt_in
   rescue
     redirect_to error_url
   end
 
   def opt_out
+    p params
     contact = Contact.find_by_random_opt_string(params["opt_in_uuid"])
+    p contact
     @success = CustomerIO::Contact.new(contact).opt_out
   rescue
     redirect_to error_url
