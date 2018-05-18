98e9b9a4782eed6ede7922f78bf86d8603f253f2
Sam Morgan <s_morgan@me.com>
2018-05-18 12:51:56 +0100

    Remove logging and redirect to errors if they error :)

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,25 +2,17 @@ require "#{Rails.root}/lib/customerio/contact"
 
 class ContactsController < ApplicationController
   def opt_in
-    p params
     contact = Contact.find_by_random_opt_string(params["opt_in_uuid"])
-    p contact
-    p CustomerIO::Contact.new(contact)
     @success = CustomerIO::Contact.new(contact).opt_in
-    p @success
-  # rescue
-  #   redirect_to error_url
+  rescue
+    redirect_to error_url
   end
 
   def opt_out
-    p params
     contact = Contact.find_by_random_opt_string(params["opt_in_uuid"])
-    p contact
-    p CustomerIO::Contact.new(contact)
     @success = CustomerIO::Contact.new(contact).opt_out
-    p @success
-  # rescue
-  #   redirect_to error_url
+  rescue
+    redirect_to error_url
   end
 
   def error
